3cebf57ea8cf88e5966cd65cdaabc6236915f515
Giovani Silva <giovani-silva1@live.com>
2024-12-30 18:52:31 -0300

    criado a operacao de instanciacao

--- loja_ionic_be/src/main/java/com/giovanidev/loja_ionic_be/LojaIonicBeApplication.java
+++ loja_ionic_be/src/main/java/com/giovanidev/loja_ionic_be/LojaIonicBeApplication.java
@@ -1,13 +1,34 @@
 package com.giovanidev.loja_ionic_be;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LojaIonicBeApplication {
+import com.giovanidev.loja_ionic_be.domain.Categoria;
+import com.giovanidev.loja_ionic_be.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class LojaIonicBeApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaIonicBeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "INFORMATICA");
+		Categoria cat2 = new Categoria(null, "ESCRITÓRIO");
+		
+	   categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
