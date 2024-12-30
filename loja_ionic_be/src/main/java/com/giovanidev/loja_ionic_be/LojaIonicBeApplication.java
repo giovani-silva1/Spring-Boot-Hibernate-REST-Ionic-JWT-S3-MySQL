a13017a4cc3e64dad81c4e82eef92454af48aa0f
Giovani Silva <giovani-silva1@live.com>
2024-12-30 17:54:03 -0300

    Projeto criado

---
+++ loja_ionic_be/src/main/java/com/giovanidev/loja_ionic_be/LojaIonicBeApplication.java
@@ -0,0 +1,13 @@
+package com.giovanidev.loja_ionic_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaIonicBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaIonicBeApplication.class, args);
+	}
+
+}
---
+++ loja_ionic_be/src/test/java/com/giovanidev/loja_ionic_be/LojaIonicBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovanidev.loja_ionic_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaIonicBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
